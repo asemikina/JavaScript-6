c17d52d393e4c421e5ba0f3909ad68a2c256d77c
AlenaSem <semickina.al@yandex.ru>
2021-05-31 22:31:54 +0600

    JavaScript-6 урок

---
+++ Задание 2/script.js
@@ -0,0 +1,50 @@
+ var items = document.getElementsByClassName('item'),
+      recycle = document.getElementById('recycle'),
+      infotext = document.getElementById('infotext'),
+      summ = document.getElementsByClassName('summ'),
+      del_button = document.getElementsByClassName('del_button')
+
+    //Функция добавления элемента в корзину
+    function addItem() {
+      //скрываем кнопку купить
+      infotext.style.display = 'none';
+      //добавляем элементы в корзину
+      var item_clone = this.parentElement.cloneNode(true);
+      recycle.appendChild(item_clone)
+
+      //Считаем общую сумму покупок
+      var prices = recycle.getElementsByClassName('price')
+      totalprice = 0;
+      for (var j = 0; j < prices.length; j++) {
+        totalprice = totalprice + Number(prices[j].innerHTML);
+        //console.log('Элемент N', j + 1, 'Сумма:', totalprice);
+      }
+      console.log('TOTAL PRICE', totalprice);
+      summ[0].innerHTML = 'TOTAL PRICE ' + totalprice;
+    }
+
+    //Обработка нажатия на кнопку для добавления элемента в корзину
+    var item_button = document.getElementsByClassName('item_button');
+    for (var i = 0; i < item_button.length; i++) {
+      item_button[i].onclick = addItem;
+    }
+    //Очистка корзины
+    var buttonClear = document.getElementById('clear');
+    buttonClear.onclick = function() {
+      //очистка начального текста корзины
+      recycle.innerHTML = ''
+      recycle.appendChild(infotext)
+      infotext.style.display = 'block';
+      //очистка блока с общей суммой покупок
+      summ[0].innerText = '';
+    }
+
+    //Функция удаления элемента из корзины
+    function delItem() {
+      this.parentElement.remove()
+    }
+
+    //Обработка нажания на кнопку для удаления элементы из корзины
+    for (var i = 0; i < del_button.length; i++) {
+      del_button[i].setAttribute('onclick', 'delItem.call(this)');
+    }
